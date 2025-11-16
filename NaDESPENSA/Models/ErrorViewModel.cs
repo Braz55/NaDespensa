f4e4a81acb496336ac00b99e67ed15fbcacd29e2
Braz55 <pm7703125@gmail.com>
2025-11-16 19:46:09 +0000

    projeto-add

---
+++ NaDESPENSA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NaDESPENSA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
